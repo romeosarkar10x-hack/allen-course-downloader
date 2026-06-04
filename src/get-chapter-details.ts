8db0d6d03e33a79c7a1b50d1a8c001737adf2aba
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-04 16:40:52 +0530

    Added chapter-details schema and fn

---
+++ src/get-chapter-details.ts
@@ -0,0 +1,38 @@
+import { fromPromise } from "neverthrow";
+import { EnvResultAsync } from "./lib/env";
+import { PRL } from "@/lib/prl";
+import { parseResponseJSON } from "./utils/parse-response-json";
+import { zodParseAsync } from "./utils/zod-parse-async";
+import { ChapterDetailsResponseSchema } from "./schemas/chapter-details";
+
+export function getChapterDetails({ subjectID, id }: { subjectID: string; id: string }) {
+    return EnvResultAsync.andThen(env => {
+        const url = "https://api.allen-live.in/api/v1/pages/getPage";
+
+        const searchParams = new URLSearchParams();
+        searchParams.append("batch_id", env.BATCH_ID.join(","));
+        searchParams.append("selected_batch_list", env.SELECTED_BATCH_LIST.join(","));
+        searchParams.append("selected_course_id", env.SELECTED_COURSE_ID);
+        searchParams.append("stream", env.STREAM);
+        searchParams.append("subject_id", subjectID);
+        searchParams.append("taxonomy", env.TAXONOMY);
+        searchParams.append("topic_id", id);
+
+        const payload = {
+            page_url: "/topic-details?" + searchParams.toString(),
+        };
+
+        const task = () =>
+            fetch(url, {
+                body: JSON.stringify(payload),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${env.BEARER_TOKEN}`,
+                },
+            });
+
+        return fromPromise(PRL.schedule(task), error => error as Error)
+            .andThen(parseResponseJSON)
+            .andThen(zodParseAsync(ChapterDetailsResponseSchema));
+    });
+}
---
+++ src/schemas/chapter-details.ts
@@ -0,0 +1,182 @@
+import z from "zod";
+
+const CardContentSchema = z
+    .object({
+        content_action: z.object({
+            data: z.object({
+                title: z.string(),
+                uri: z.url(),
+            }),
+        }),
+    })
+    .transform(
+        ({
+            content_action: {
+                data: { title, uri },
+            },
+        }) => ({ name: title, url: uri }),
+    );
+
+const CardSchema = z
+    .object({
+        card_action: z.object({
+            data: z.object({
+                title: z.string(),
+                uri: z.url(),
+            }),
+        }),
+    })
+    .transform(
+        ({
+            card_action: {
+                data: { title, uri },
+            },
+        }) => ({ name: title, url: uri }),
+    );
+
+const CardWithContentSchema = z
+    .object({
+        card_action: z.object({
+            data: z.object({
+                content: z.object({
+                    data: z.object({
+                        contents_list: z.array(CardContentSchema),
+                    }),
+                }),
+            }),
+            tracking_params: z.object({ current: z.object({ card_name: z.string() }) }),
+        }),
+    })
+    .transform(
+        ({
+            card_action: {
+                data: {
+                    content: {
+                        data: { contents_list },
+                    },
+                },
+                tracking_params: {
+                    current: { card_name },
+                },
+            },
+        }) => ({ name: card_name, $: contents_list }),
+    );
+
+/*
+const ChapterSchema = z
+    .object({
+        action: z.object({
+            data: z.object({
+                query: z.object({
+                    topic_id: z.string(),
+                }),
+                uri: z.string(),
+            }),
+            tracking_params: z.object({
+                current: z.object({
+                    topic_name: z.string(),
+                    subject_id: z.string(),
+                }),
+            }),
+        }),
+    })
+    .transform(
+        ({
+            action: {
+                data: {
+                    query: { topic_id },
+                },
+                tracking_params: {
+                    current: { topic_name, subject_id },
+                },
+            },
+        }) => ({ id: topic_id, name: topic_name, subjectID: subject_id, $chapter: true }),
+    );
+    
+    */
+
+export const ContentSchema = z
+    .object({
+        content_action: z.object({
+            data: z.object({
+                title: z.string(),
+                uri: z.url(),
+            }),
+        }),
+    })
+    .transform(
+        ({
+            content_action: {
+                data: { title, uri },
+            },
+        }) => ({ name: title, url: uri }),
+    );
+
+const SelectionCardSchema = z.object({
+    type: z.literal("SELECTION_CARD"),
+});
+const AppGenericHeaderV2Schema = z.object({
+    type: z.literal("APP_GENERIC_HEADER_V2"),
+});
+
+const BreadCrumbsWidgetSchema = z.object({
+    type: z.literal("BREADCRUMBS"),
+});
+
+export const PolymorphicWidgetSchema = z.object({
+    type: z.literal("POLYMORPHIC_WIDGET"),
+    data: z.object({
+        data: z
+            .union([
+                z.object({
+                    contents_list: z.array(ContentSchema),
+                    title: z.string(),
+                }),
+                z.object({
+                    cards: z.array(z.union([CardSchema, CardWithContentSchema])),
+                    title: z.string(),
+                }),
+                z.object({}) as z.ZodType<{}>,
+            ])
+            .exactOptional(),
+    }),
+});
+
+export const WidgetSchema = z.discriminatedUnion("type", [
+    BreadCrumbsWidgetSchema,
+    PolymorphicWidgetSchema,
+    AppGenericHeaderV2Schema,
+    SelectionCardSchema,
+]);
+
+const PageContentSchema = z.object({
+    widgets: z.array(WidgetSchema).transform(array =>
+        array
+            .filter(element => element.type === "POLYMORPHIC_WIDGET")
+            .flatMap(element => ("data" in element.data ? [element.data.data] : []))
+            .filter(element => "cards" in element || "contents_list" in element)
+            .map(element => ({ $: "cards" in element ? element.cards : element.contents_list, name: element.title })),
+    ),
+});
+
+const DataObjectSchema = z
+    .object({
+        page_content: PageContentSchema,
+    })
+    .transform(data => ({
+        pageContent: data.page_content,
+    }));
+
+export const ChapterDetailsResponseSchema = z
+    .object({
+        status: z.number(),
+        reason: z.string(),
+        data: DataObjectSchema,
+    })
+    .transform(
+        ({
+            data: {
+                pageContent: { widgets },
+            },
+        }) => widgets,
+    );
