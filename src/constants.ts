fd67621c4ea26d94796e80013fb3daa09f5128e3
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-04 16:59:27 +0530

    Added constants

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const ALLEN_API_URL = "https://api.allen-live.in/api/v1/pages/getPage";
+
+export const commonHeaders = {
+    "X-Client-Type": "web",
+    "X-Locale": "en",
+    "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+};
+
+export const subjects = [
+    { name: "Physics", id: "1160" },
+    { name: "Chemistry", id: "746" },
+    { name: "Mathematics", id: "1264" },
+];
