6ff2bee0743d97884fa5c897beb21f38a8b48e26
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-04 16:57:36 +0530

    Added recursive content types

---
+++ src/types/node-types.ts
@@ -0,0 +1,30 @@
+/* export type RecursiveContentType<T> = {
+    $?: RecursiveContentType<T>[];
+} & T; */
+
+export type ContentLeafNodeType = {
+    name: string;
+    url: string;
+};
+
+export type ChapterLeafNodeType = {
+    id: string;
+    name: string;
+    $chapter: true;
+    subjectID: string;
+};
+
+export type ContentNodeType =
+    | {
+          name: string;
+          $?: ContentNodeType[];
+      }
+    | ContentLeafNodeType;
+
+export type ChapterContentNodeType =
+    | {
+          name: string;
+          $?: ChapterContentNodeType[];
+      }
+    | ContentLeafNodeType
+    | ChapterLeafNodeType;
