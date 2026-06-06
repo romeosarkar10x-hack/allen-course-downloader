2c288b1107f9391916130f347aeae0da7d9a808b
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-07 00:37:34 +0530

    Changed function to accept everything in params

--- src/get-subject-details.ts
+++ src/get-subject-details.ts
@@ -1,14 +1,32 @@
 import { fromPromise, okAsync, ResultAsync } from "neverthrow";
-import { EnvResultAsync } from "./lib/env";
 import { PP } from "@/lib/pp";
 import { parseResponseJSON } from "./utils/parse-response-json";
 import { zodParseAsync } from "./utils/zod-parse-async";
 import { SubjectDetailsResponseSchema } from "./schemas/subject-details";
-import type { ChapterContentNodeType, ContentNodeType } from "./types/node-types";
+import type { ChapterContentNodeType, ChapterLeafNodeType, ContentLeafNodeType } from "./types/node-types";
 import { getChapterDetails } from "./get-chapter-details";
 import { commonHeaders } from "./constants";
+import { dedupeTree } from "./utils/dedupe-tree";
 
-export function getSubjectDetails({ id, name }: { id: string; name: string }) {
+export function getSubjectDetails({
+    id,
+    name,
+    batchIDs,
+    stream,
+    selectedBatchList,
+    selectedCourseID,
+    taxonomy,
+    bearerToken,
+}: {
+    id: string;
+    name: string;
+    stream: string;
+    batchIDs: string[];
+    selectedBatchList: string[];
+    taxonomy: string;
+    selectedCourseID: string;
+    bearerToken: string;
+}) {
     function appendChapterDetailsRecursively(content: ChapterContentNodeType) {
         if ("$" in content) {
             ResultAsync.combine(content.$.map(appendChapterDetailsRecursively)).map(result => ({
@@ -18,43 +36,53 @@ export function getSubjectDetails({ id, name }: { id: string; name: string }) {
         }
 
         if ("$chapter" in content) {
-            return getChapterDetails({ subjectID: content.subjectID, id: content.id, name });
+            return getChapterDetails({
+                subjectID: content.subjectID,
+                id: content.id,
+                name,
+                batchIDs,
+                selectedBatchList,
+                selectedCourseID,
+                bearerToken,
+                taxonomy,
+                stream,
+            });
         }
 
         return okAsync(content);
     }
 
-    return EnvResultAsync.andThen(env => {
-        const url = "https://api.allen-live.in/api/v1/pages/getPage";
+    const url = "https://api.allen-live.in/api/v1/pages/getPage";
 
-        const searchParams = new URLSearchParams();
-        searchParams.append("batch_id", env.BATCH_ID.join(","));
-        searchParams.append("selected_batch_list", env.SELECTED_BATCH_LIST.join(","));
-        searchParams.append("selected_course_id", env.SELECTED_COURSE_ID);
-        searchParams.append("stream", env.STREAM);
-        searchParams.append("subject_id", id);
-        searchParams.append("taxonomy_id", env.TAXONOMY);
+    const searchParams = new URLSearchParams();
+    searchParams.append("batch_id", batchIDs.join(","));
+    searchParams.append("selected_batch_list", selectedBatchList.join(","));
+    searchParams.append("selected_course_id", selectedCourseID);
+    searchParams.append("stream", stream);
+    searchParams.append("subject_id", id);
+    searchParams.append("taxonomy_id", taxonomy);
 
-        const payload = {
-            page_url: "/subject-details?" + searchParams.toString(),
-        };
+    const payload = {
+        page_url: "/subject-details?" + searchParams.toString(),
+    };
 
-        const task = () =>
-            fetch(url, {
-                method: "POST",
-                body: JSON.stringify(payload),
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${env.BEARER_TOKEN}`,
-                    ...commonHeaders,
-                },
-            });
+    const task = () =>
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${bearerToken}`,
+                ...commonHeaders,
+            },
+        });
 
-        const taskMetadata = { fetch: { path: "/subject-details", subjectID: id } };
+    const taskMetadata = { fetch: { path: "/subject-details", subjectID: id, name } };
 
-        return fromPromise(PP.schedule(task, taskMetadata).promise, error => error as Error)
-            .andThen(parseResponseJSON)
-            .andThen(zodParseAsync(SubjectDetailsResponseSchema))
-            .andThen(subjectDetails => appendChapterDetailsRecursively({ $: subjectDetails, name }));
-    });
+    return fromPromise(PP.schedule(task, taskMetadata).promise, error => error as Error)
+        .andThen(parseResponseJSON)
+        .andThen(zodParseAsync(SubjectDetailsResponseSchema))
+        .map($ => ({ $, name }))
+        .map(t => dedupeTree<{ name: string } | ChapterLeafNodeType | ContentLeafNodeType>(t))
+        .andThen(appendChapterDetailsRecursively);
 }
