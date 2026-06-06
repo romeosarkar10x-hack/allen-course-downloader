1d38a6798a6b6ce93a8a06bd40fad06a5c7df013
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-07 01:46:53 +0530

    Removed some env variables. They will now come from the getStudentDetails response

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,11 +4,8 @@ import { zodParseAsync } from "@/utils/zod-parse-async";
 
 const EnvSchema = z.object({
     BEARER_TOKEN: z.jwt(),
-    STREAM: z.string(),
     TAXONOMY: z.string(),
     SELECTED_COURSE_ID: z.string(),
-    SELECTED_BATCH_LIST: z.string().transform(list => list.split(",")),
-    BATCH_ID: z.string().transform(list => list.split(",")),
 });
 
 export const EnvResultAsync = zodParseAsync(EnvSchema)(process.env);
