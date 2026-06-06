27449958b82170826a43a2230a8a007eb05e141f
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-07 00:35:50 +0530

    Changed function to accept everything in params instead of env variables

--- src/get-chapter-details.ts
+++ src/get-chapter-details.ts
@@ -6,42 +6,60 @@ import { zodParseAsync } from "./utils/zod-parse-async";
 import { ChapterDetailsResponseSchema } from "./schemas/chapter-details";
 import { commonHeaders } from "./constants";
 
-export function getChapterDetails({ subjectID, id, name }: { subjectID: string; id: string; name: string }) {
-    return EnvResultAsync.andThen(env => {
-        const url = "https://api.allen-live.in/api/v1/pages/getPage";
+export function getChapterDetails({
+    id,
+    name,
+    subjectID,
+    batchIDs,
+    selectedBatchList,
+    selectedCourseID,
+    stream,
+    taxonomy,
+    bearerToken,
+}: {
+    id: string;
+    name: string;
+    stream: string;
+    taxonomy: string;
+    batchIDs: string[];
+    subjectID: string;
+    selectedBatchList: string[];
+    selectedCourseID: string;
+    bearerToken: string;
+}) {
+    const url = "https://api.allen-live.in/api/v1/pages/getPage";
 
-        const searchParams = new URLSearchParams();
-        searchParams.append("batch_id", env.BATCH_ID.join(","));
-        searchParams.append("selected_batch_list", env.SELECTED_BATCH_LIST.join(","));
-        searchParams.append("selected_course_id", env.SELECTED_COURSE_ID);
-        searchParams.append("stream", env.STREAM);
-        searchParams.append("subject_id", subjectID);
-        searchParams.append("taxonomy_id", env.TAXONOMY);
-        searchParams.append("topic_id", id);
+    const searchParams = new URLSearchParams();
+    searchParams.append("batch_id", batchIDs.join(","));
+    searchParams.append("selected_batch_list", selectedBatchList.join(","));
+    searchParams.append("selected_course_id", selectedCourseID);
+    searchParams.append("stream", stream);
+    searchParams.append("subject_id", subjectID);
+    searchParams.append("taxonomy_id", taxonomy);
+    searchParams.append("topic_id", id);
 
-        const payload = {
-            page_url: "/topic-details?" + searchParams.toString(),
-        };
+    const payload = {
+        page_url: "/topic-details?" + searchParams.toString(),
+    };
 
-        const task = () =>
-            fetch(url, {
-                method: "POST",
-                body: JSON.stringify(payload),
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${env.BEARER_TOKEN}`,
-                    ...commonHeaders,
-                },
-            });
+    const task = () =>
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${bearerToken}`,
+                ...commonHeaders,
+            },
+        });
 
-        const taskMetadata = { fetch: { path: "/topic-details", subjectID, topicID: id } };
+    const taskMetadata = { fetch: { path: "/topic-details", subjectID, topicID: id } };
 
-        return fromPromise(PP.schedule(task, taskMetadata).promise, error => error as Error)
-            .andThen(parseResponseJSON)
-            .andThen(zodParseAsync(ChapterDetailsResponseSchema))
-            .map(details => ({
-                $: details,
-                name,
-            }));
-    });
+    return fromPromise(PP.schedule(task, taskMetadata).promise, error => error as Error)
+        .andThen(parseResponseJSON)
+        .andThen(zodParseAsync(ChapterDetailsResponseSchema))
+        .map(details => ({
+            $: details,
+            name,
+        }));
 }
