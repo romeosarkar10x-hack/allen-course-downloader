e1d79dfaea0222ab775723c292c2155f60499fce
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-07 00:38:36 +0530

    Similar to the other 2; Changed fn to accept everything in params

--- src/get-course-details.ts
+++ src/get-course-details.ts
@@ -1,6 +1,37 @@
 import { ResultAsync } from "neverthrow";
 import { getSubjectDetails } from "./get-subject-details";
 
-export function getCourseDetails(subjects: { name: string; id: string }[]) {
-    return ResultAsync.combine(subjects.map(getSubjectDetails));
+export function getCourseDetails({
+    name,
+    subjects,
+    batchIDs,
+    selectedBatchList,
+    selectedCourseID,
+    stream,
+    taxonomy,
+    bearerToken,
+}: {
+    subjects: { name: string; id: string }[];
+    name: string;
+    stream: string;
+    taxonomy: string;
+    batchIDs: string[];
+    selectedBatchList: string[];
+    selectedCourseID: string;
+    bearerToken: string;
+}) {
+    return ResultAsync.combine(
+        subjects.map(({ name, id }) =>
+            getSubjectDetails({
+                id,
+                name,
+                batchIDs,
+                selectedBatchList,
+                selectedCourseID,
+                stream,
+                taxonomy,
+                bearerToken,
+            }),
+        ),
+    ).map($ => ({ $, name }));
 }
