636fcf9f06d7c1cf9ed6383ac41537746502a9ea
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-07 00:39:13 +0530

    Added more headers

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,19 @@
 export const ALLEN_API_URL = "https://api.allen-live.in/api/v1/pages/getPage";
 
 export const commonHeaders = {
-    "X-Client-Type": "web",
-    "X-Locale": "en",
+    "Cache-Control": "no-cache",
+    "Origin": "https://allen.in",
+    "Referer": "https://allen.in/",
+    "Sec-Ch-Ua": 'Chromium";v="148", "Google Chrome";v="148", "Not/A)Brand";v="99',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "Windows",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "cross-site",
     "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+    "X-Client-Type": "web",
+    "X-Locale": "en",
 };
 
 export const subjects = [
