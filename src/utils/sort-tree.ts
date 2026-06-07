aa45722f13f41ec3aca0dc205daece323af771bd
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-07 17:45:35 +0530

    Updated sortTree to work accordingly with the new TreeNode type

--- src/utils/sort-tree.ts
+++ src/utils/sort-tree.ts
@@ -1,6 +1,9 @@
 import type { TreeNode } from "@/types/tree-node";
 
-export function sortTree<T extends object>(root: TreeNode<T>, compareFn: (a: TreeNode<T>, b: TreeNode<T>) => number) {
+export function sortTree<T extends object & { $?: never }>(
+    root: TreeNode<T>,
+    compareFn: (a: TreeNode<T>, b: TreeNode<T>) => number,
+) {
     (function sortTreeRecursive(node: TreeNode<T>) {
         if ("$" in node) {
             const childNodes = node.$;
