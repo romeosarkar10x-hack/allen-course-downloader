3d31042e50b840ea3c09bd1a8bbb7f4309b5c549
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-07 12:40:10 +0530

    Updated dedupeTree fn based on new TreeNode type

--- src/utils/dedupe-tree.ts
+++ src/utils/dedupe-tree.ts
@@ -1,11 +1,14 @@
 import type { TreeNode } from "@/types/tree-node";
 import lodash from "lodash";
 
-export function dedupeTree<T extends object>(node: TreeNode<T>): TreeNode<T> {
-    if (!("$" in node)) {
-        return node;
+export function dedupeTree<
+    InternalNode extends object & { $?: never },
+    LeafNode extends object & { $?: never } = InternalNode,
+>(node: TreeNode<InternalNode, LeafNode>): TreeNode<InternalNode, LeafNode> {
+    if ("$" in node) {
+        const deduped = node.$.map(dedupeTree);
+        return { ...node, $: lodash.uniqWith(deduped, lodash.isEqual) };
     }
 
-    const deduped = node.$.map(dedupeTree);
-    return { ...node, $: lodash.uniqWith(deduped, lodash.isEqual) };
+    return node;
 }
