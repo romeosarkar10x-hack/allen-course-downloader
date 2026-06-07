f0a757ac97bc6688da8f53d0c48a883b839c3ca6
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-07 18:28:29 +0530

    Added coverage directory to ignore list

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-    globalIgnores(["dist"]),
+    globalIgnores(["dist", "coverage"]),
     {
         files: ["**/*.{ts}"],
         extends: [js.configs.recommended, tseslint.configs.recommended],
