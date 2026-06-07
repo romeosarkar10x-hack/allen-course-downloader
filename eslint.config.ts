86e6597ac38a354e1c6976e6273fa451ca763c1d
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-07 18:29:18 +0530

    Fixed error. {} (curly brace syntax) is for multiple extensions. With single value, it doesn't match anything

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 export default defineConfig([
     globalIgnores(["dist", "coverage"]),
     {
-        files: ["**/*.{ts}"],
+        files: ["**/*.ts"],
         extends: [js.configs.recommended, tseslint.configs.recommended],
         languageOptions: {
             globals: globals.browser,
