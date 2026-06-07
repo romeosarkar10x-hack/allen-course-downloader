c6ec789a921696976eae1c46a8b22779470109b1
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-07 19:21:16 +0530

    Added eslint exception to ignore variables / args starting with _ (underscores)

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,5 +11,14 @@ export default defineConfig([
         languageOptions: {
             globals: globals.browser,
         },
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                },
+            ],
+        },
     },
 ]);
