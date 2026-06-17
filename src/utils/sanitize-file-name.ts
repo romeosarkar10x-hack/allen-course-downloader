175f2ea35c3376ba7fc6b5aabbce90f409f474fb
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-18 00:35:35 +0530

    Fixed bug

--- src/utils/sanitize-file-name.ts
+++ src/utils/sanitize-file-name.ts
@@ -7,7 +7,7 @@ export function sanitizeFileName(fileName: string) {
     for (let i = 0; i < fileName.length; i++) {
         const char = fileName[i]!;
 
-        if (!'<>:"*?|\\/'.includes(char) && (char.charCodeAt(0) <= " ".charCodeAt(0) || char.charCodeAt(0) <= 127)) {
+        if (!'<>:"*?|\\/'.includes(char) && (" ".charCodeAt(0) <= char.charCodeAt(0) || char.charCodeAt(0) < 127)) {
             sanitized += char;
         }
     }
